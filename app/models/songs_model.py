2f2f816be0af776a1b5682dcf2ff9dbc7bcd56b9
Shruti Bhati <sb.shrutibhati@gmail.com>
2016-03-31 03:04:05 +0000

    fixes

--- app/models/songs_model.py
+++ app/models/songs_model.py
@@ -1,13 +1,14 @@
 from app import db
 class songs:
 	def __init__(self):
-		self.attributes = ['id','artist','song','uri']
+		self.attributes = ['song_id','artist','song','uri']
 		self.database = 'musicon'
 		self.collection = 'songs'
 
 	def get(self,ids):
 		print "fetching songs with ids",ids
-		cursor_object  = db[self.database][self.collection].find({"id":{"$in":ids}})
+		ids = [int(i) for i in ids]
+		cursor_object  = db[self.database][self.collection].find({"song_id":{"$in":ids}})
 		uris = list()
 		for doc in cursor_object:
 			uris.append(doc['uri'])
