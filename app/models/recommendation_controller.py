ba1f6e3a0ae11f7645bd2a303529159ed256b049
Shruti Bhati <sb.shrutibhati@gmail.com>
2016-04-12 01:54:58 +0000

    fixes

--- app/models/recommendation_controller.py
+++ app/models/recommendation_controller.py
@@ -14,14 +14,14 @@ class recommendation:
 
 	def get_similar_tracks(self,song_data,num):
 		similar = []
-		track = urllib.urlencode(song_data[0]['track'])
-		artist = urllib.urlencode(song_data[0]['artist'])
+		track = urllib.quote_plus(song_data[0]['track'])
+		artist = urllib.quote_plus(song_data[0]['artist'])
 		response = requests.get("http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist="+artist+"&track="+track+"&api_key="+self.lastfm_api_key+"&format=json")
 		response = response.json()
 		tracks = response['similartracks']['track']
 		if len(tracks):
 			filtered_tracks = tracks[:num]
-			filtered_tracks = [{"track":a['name'],"artist":a['artist']['name'] for a in filtered_tracks}			
+			filtered_tracks = [{"track":a['name'],"artist":a['artist']['name']} for a in filtered_tracks]
 		else:
 			artist_response = requests.get("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist="+artist+"&api_key="+self.lastfm_api_key+"&format=json")
 			artist_response = artist_response.json()
@@ -30,10 +30,9 @@ class recommendation:
 				if len(artists) < num:
 					num = len(artists)
 				for i in range(num):
-					sim_artist = urllib.urlencode(artists[i]['name'])
+					sim_artist = urllib.quote_plus(artists[i]['name'])
 					top_tracks_response = requests.get("http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist="+sim_artist+"&api_key="+self.lastfm_api_key+"&format=json")
 					top_tracks_response = top_tracks_response.json()
 					tracks = top_tracks_response['toptracks']['track'][0]
 					similar.append({"track":tracks['name'],"artist":tracks['artist']['name']})					
-		print similar
 		return similar
